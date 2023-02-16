9ed1e00a7303350f7893ae1b53b924fa03593167
Noah Clark <noahammon00@gmail.com>
2023-02-15 18:59:54 -0700

    initial commit

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+function isPalindrome(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  };
+  if (newStr.toLowerCase() === str.toLowerCase()) {
+    return 'Is a Palindrome';
+  } else {
+    return 'Is not a Palindrome';
+  }
+};
+
+console.log(isPalindrome('raceca'));
